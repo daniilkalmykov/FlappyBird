2f373cdecffab5840e45ed6161399a258f150298
daniilkalmykov <daniilk24960@gmail.com>
2023-09-18 22:17:38 +0600

    add player data

---
+++ Assets/Sources/PlayerData/PlayerData.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Sources.PlayerData
+{
+    public static class PlayerData
+    {
+        private const string LevelSettings = "LevelSettings";
+
+        public static void SetLevelSettings(string levelSettings)
+        {
+            PlayerPrefs.SetString(LevelSettings, levelSettings);
+        }
+
+        public static string GetLevelSettings()
+        {
+            return PlayerPrefs.GetString(LevelSettings);
+        }
+    }
+}
