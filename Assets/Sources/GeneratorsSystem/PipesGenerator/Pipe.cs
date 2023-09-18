a68864ccabb1e6759a17c63d29082392460cbee8
daniilkalmykov <daniilk24960@gmail.com>
2023-09-18 21:58:22 +0600

    add level settings

--- Assets/Sources/GeneratorsSystem/PipesGenerator/Pipe.cs
+++ Assets/Sources/GeneratorsSystem/PipesGenerator/Pipe.cs
@@ -1,5 +1,7 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
+[assembly: InternalsVisibleTo("LevelSettings")]
 namespace Sources.GeneratorsSystem.PipesGenerator
 {
     internal sealed class Pipe : MonoBehaviour
---
+++ Assets/Sources/LevelSettings/LevelSettings.cs
@@ -0,0 +1,14 @@
+using Sources.GeneratorsSystem.PipesGenerator;
+using UnityEngine;
+
+namespace Sources.LevelSettings
+{
+    [CreateAssetMenu(fileName = "New Level Settings", menuName = "Settings/Level Settings")]
+    internal sealed class LevelSettings : ScriptableObject
+    {
+        [field: SerializeField] public float PlayerSpeed { get; private set; }
+        [field: SerializeField] public float Delay { get; private set; }
+        [field: SerializeField] public int PipesCount { get; private set; }
+        [field: SerializeField] public Pipe Prefab { get; private set; }
+    }
+}
