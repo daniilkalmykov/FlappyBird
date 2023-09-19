a595a8aaedc316c73969d6ab783644583ab3f804
daniilkalmykov <daniilk24960@gmail.com>
2023-09-19 20:09:19 +0600

    add button view

---
+++ Assets/Sources/Views/ButtonView.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Sources.Views
+{
+    [RequireComponent(typeof(Button))]
+    internal sealed class ButtonView : MonoBehaviour
+    {
+        private Button _button;
+
+        public event Action Clicked;
+
+        private void OnEnable()
+        {
+            if (_button != null)
+                _button.onClick.AddListener(() => Clicked?.Invoke());
+        }
+
+        private void OnDisable()
+        {
+            _button.onClick.RemoveListener(() => Clicked?.Invoke());
+        }
+
+        public void Init()
+        {
+            _button = GetComponent<Button>();
+
+            _button.onClick.AddListener(() => Clicked?.Invoke());
+        }
+    }
+}
